de3465df97f17da01650392afe7a0e446c19bd40
sudharsanramakrishnan <36293360+sudharsanramakrishnan@users.noreply.github.com>
2018-02-09 16:06:39 +0530

    Update gu.c

--- gu.c
+++ gu.c
@@ -1,17 +1,5 @@
-#include <stdio.h>
-int main()
-{
- int a[50],n,i;
-  scanf("%d",&n);
-  for(i=0;i<n;i++)
-  {
-    scanf("%d",&a[i]);
-  }
-  for(i=0;i<n;i++)
-  {
-    if(a[i]<0)
-      printf("%d %d",a[i],(-a[i]));
-      
-  }
- return 0;
-}
+n=int(input("Enter number: "))
+if(n>0):
+    print("Number is positive")
+else:
+    print("Number is negative")
