68123a3856bd0a84750f3a8dd072ea28557dfb2b
sudharsanramakrishnan <36293360+sudharsanramakrishnan@users.noreply.github.com>
2018-02-09 17:56:16 +0530

    Update se.py

--- se.py
+++ se.py
@@ -1,11 +1,11 @@
-num1 = 10
-num2 = 14
-num3 = 12
-if (num1 >= num2) and (num1 >= num3):
-   largest = num1
-elif (num2 >= num1) and (num2 >= num3):
+num = 55
+num2 = 24
+num3 = 34
+if (num >= num2) and (num >= num3):
+   largest = num
+elif (num2 >= num) and (num2 >= num3):
    largest = num2
 else:
    largest = num3
 
-print("The largest number between",num1,",",num2,"and",num3,"is",largest)
+print("The largest number between",num,",",num2,"and",num3,"is",largest)
